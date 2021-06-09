a496a2af2df6d18106ded58876701141894fe6c7
Paige <72361304+pckellyco@users.noreply.github.com>
2021-06-09 14:05:05 -0600

    deploying with heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:username, :password)
+        params.require(:user).permit(:username, :password, :authorized_user)
     end
 
 end
